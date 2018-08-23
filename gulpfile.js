d2b79852299c19a8e237b17af95c94b7334b9118
Steve Szczecina <steve@steves-mbp.lan>
2018-08-22 21:27:26 -0600

    update kids

--- gulpfile.js
+++ gulpfile.js
@@ -1,118 +1,123 @@
-var gulp            = require('gulp'),
-    include         = require('gulp-file-include'),
-    sass            = require('gulp-sass'),
-    autoprefixer    = require('gulp-autoprefixer'),
-    minifycss       = require('gulp-cssnano'),
-    rename          = require('gulp-rename'),
-    eslint          = require('gulp-eslint'),
-    source          = require('vinyl-source-stream'),
-    buffer          = require('vinyl-buffer'),
-    uglify          = require('gulp-uglify'),
-    browserify      = require('browserify'),
-    watchify        = require('watchify'),
-    babelify        = require('babelify');
+var gulp = require("gulp"),
+  include = require("gulp-file-include"),
+  sass = require("gulp-sass"),
+  autoprefixer = require("gulp-autoprefixer"),
+  minifycss = require("gulp-cssnano"),
+  rename = require("gulp-rename"),
+  eslint = require("gulp-eslint"),
+  source = require("vinyl-source-stream"),
+  buffer = require("vinyl-buffer"),
+  uglify = require("gulp-uglify"),
+  browserify = require("browserify"),
+  watchify = require("watchify"),
+  babelify = require("babelify");
 
 var path = {
-        src: {
-            html: './src/html/pages/*.html',
-            includes: 'src/html/includes/',
-            scss: './src/scss/manifest.scss',
-            img: './src/img/*',
-            js  : './src/js/**/*',
-            entry: './src/js/app.js',
-            public: './src/public/*'
-        },
-        dist: {
-            html: './docs',
-            css: './docs/css',
-            js  : './docs/js',
-            img: './docs/img',
-            public: './docs'
-        },
-        file: {
-            app: {
-                dev: 'app.js',
-                prod: 'app.min.js'
-            }
-        }
-    };
+  src: {
+    html: "./src/html/pages/*.html",
+    includes: "src/html/includes/",
+    scss: "./src/scss/manifest.scss",
+    img: "./src/img/*",
+    js: "./src/js/**/*",
+    entry: "./src/js/app.js",
+    public: "./src/public/*"
+  },
+  dist: {
+    html: "./docs",
+    css: "./docs/css",
+    js: "./docs/js",
+    img: "./docs/img",
+    public: "./docs"
+  },
+  file: {
+    app: {
+      dev: "app.js",
+      prod: "app.min.js"
+    }
+  }
+};
 
-gulp.task('html', function(){
-    return gulp.src(path.src.html)
-    .pipe(include({prefix:'@@', basepath: path.src.includes}))
-    .pipe(gulp.dest(path.dist.html))
-;});
-
-gulp.task('scss', function() {
-    return gulp.src(path.src.scss)
-    .pipe(sass({errLogToConsole: true}))
-    .pipe(autoprefixer({browser: ['> 1%', 'last 2 versions']}))
-    .pipe(rename({basename: 'main'}))
-    .pipe(gulp.dest(path.dist.css))
-    .pipe(minifycss())
-    .pipe(rename({basename: 'main', suffix: '.min'}))
-    .pipe(gulp.dest(path.dist.css))
-    //.pipe(reload({stream:true}))
-;});
-
-gulp.task('img', function() {
-    return gulp.src(path.src.img)
-    .pipe(gulp.dest(path.dist.img))
+gulp.task("html", function() {
+  return gulp
+    .src(path.src.html)
+    .pipe(include({ prefix: "@@", basepath: path.src.includes }))
+    .pipe(gulp.dest(path.dist.html));
 });
 
-gulp.task('lint', function() {
-    return gulp.src(path.src.js)
+gulp.task("scss", function() {
+  return gulp
+    .src(path.src.scss)
+    .pipe(sass({ errLogToConsole: true }))
+    .pipe(autoprefixer({ browsers: ["> 1%", "last 2 versions"] }))
+    .pipe(rename({ basename: "main" }))
+    .pipe(gulp.dest(path.dist.css))
+    .pipe(minifycss())
+    .pipe(rename({ basename: "main", suffix: ".min" }))
+    .pipe(gulp.dest(path.dist.css));
+  //.pipe(reload({stream:true}))
+});
+
+gulp.task("img", function() {
+  return gulp.src(path.src.img).pipe(gulp.dest(path.dist.img));
+});
+
+gulp.task("lint", function() {
+  return gulp
+    .src(path.src.js)
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failAfterError());
-;});
+});
 
-gulp.task('js', function(){
-    process.env.NODE_ENV = "production";
-    browserify({
-        extensions: ['.jsx'],
-        transform: [babelify],
-        entries: [path.src.entry]
-    })
+gulp.task("js", function() {
+  process.env.NODE_ENV = "production";
+  browserify({
+    extensions: [".jsx"],
+    transform: [babelify],
+    entries: [path.src.entry]
+  })
     .bundle()
-    .on('error', console.error.bind(console))
+    .on("error", console.error.bind(console))
     .pipe(source(path.file.app.prod))
     .pipe(buffer())
     .pipe(uglify())
     .pipe(gulp.dest(path.dist.js));
 });
 
-gulp.task('public', function() {
-    return gulp.src(path.src.public)
-    .pipe(gulp.dest(path.dist.public))
-;});
+gulp.task("public", function() {
+  return gulp.src(path.src.public).pipe(gulp.dest(path.dist.public));
+});
 
-gulp.task('build',['html','scss','img','lint','js', 'public']);
+gulp.task("build", ["html", "scss", "img", "lint", "js", "public"]);
 
-gulp.task('dev', ['html', 'scss'], function() {
-    gulp.watch(path.src.html, ['html']);
-    gulp.watch(path.src.scss, ['scss']);
-    gulp.watch(path.src.js, ['lint']);
+gulp.task("dev", ["html", "scss"], function() {
+  gulp.watch(path.src.html, ["html"]);
+  gulp.watch(path.src.scss, ["scss"]);
+  gulp.watch(path.src.js, ["lint"]);
 
-    console.log('Watching and waiting...');
+  console.log("Watching and waiting...");
 
-    process.env.NODE_ENV = "development";
-    var b = watchify(browserify({
-        extensions: ['.jsx'],
-        entries: [path.src.entry],
-        transform: [babelify],
-        debug: true,
-        cache: {},
-        packageCache: {},
-        fullPaths: true
-    }), {poll:100});
+  process.env.NODE_ENV = "development";
+  var b = watchify(
+    browserify({
+      extensions: [".jsx"],
+      entries: [path.src.entry],
+      transform: [babelify],
+      debug: true,
+      cache: {},
+      packageCache: {},
+      fullPaths: true
+    }),
+    { poll: 100 }
+  );
 
-    return b.on('update', function () {
-        b.bundle()
-        .on('error', console.error.bind(console))
+  return b
+    .on("update", function() {
+      b.bundle()
+        .on("error", console.error.bind(console))
         .pipe(source(path.file.app.dev))
-        .pipe(gulp.dest(path.dist.js))
-        console.log('Bundle updated');
+        .pipe(gulp.dest(path.dist.js));
+      console.log("Bundle updated");
     })
     .bundle()
     .pipe(source(path.file.app.dev))
